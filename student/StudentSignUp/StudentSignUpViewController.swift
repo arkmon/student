cd472b133b04f41c472402492c0f6e5ad54f33a5
Arkadiusz Dowejko <Arkadiusz.Dowejko@experian.com>
2017-10-09 10:28:55 +0100

    Not too cool uipickerView

--- student/StudentSignUp/StudentSignUpViewController.swift
+++ student/StudentSignUp/StudentSignUpViewController.swift
@@ -8,24 +8,44 @@
 
 import UIKit
 
-class StudentSignUpViewController: UIViewController {
+class StudentSignUpViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var genderTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var universityTextField: UITextField!
-    
+
+
     var viewModel = StudentSignUpViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var pickerView = UIPickerView()
+        pickerView.delegate = self
+        genderTextField.inputView = pickerView
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return viewModel.genderArray.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return viewModel.genderArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        genderTextField.text = viewModel.genderArray[row]
     }
 }
