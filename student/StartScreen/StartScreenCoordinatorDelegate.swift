7ebf9d89f4a99baeee117394a67f0f67ff447f7f
Arkadiusz <arkadiuszdowejko@Arkadiuszs-iMac.local>
2017-10-14 17:41:53 +0100

    missing

---
+++ student/StartScreen/StartScreenCoordinatorDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  StartScreenCoordinatorDelegate.swift
+//  student
+//
+//  Created by Arkadiusz Dowejko on 14/10/2017.
+//  Copyright © 2017 arkmon. All rights reserved.
+//
+
+import Foundation
+
+public protocol StartScreenCoordinatorDelegate: class {
+    func dismiss()
+}
